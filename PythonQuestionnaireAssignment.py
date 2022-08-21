339499c37f5da6e9fc4caa1b282d541e5e3c2de2
Augustine <106200215+augustine24@users.noreply.github.com>
2022-08-21 18:11:19 -0400

    Update PythonQuestionnaireAssignment.py

--- PythonQuestionnaireAssignment.py
+++ PythonQuestionnaireAssignment.py
@@ -1,4 +1,3 @@
-
 import time
  
 correct = 0
@@ -16,7 +15,6 @@ if answer == "yes":
     print("Great! Let's proceed.")
 else: 
     print("Take your time and prepare. Please restart the terminal when you're ready.")
-    clear
 if answer == "yes":
     print("Alright! Question one! Are lions from the canine animal family? Type yes or no as an answer.")
 answer = input("")
@@ -25,7 +23,7 @@ if answer == "yes":
     print("That is incorrect. You have gotten" ,correct, "question(s) right so far.")
 if answer == "no":
     correct += 1
-    print("Correct! Lions are from the feline animal family. You have gotten"  ,correct, "question(s) right.")
+    print("Correct! Lions are from the feline animal family. You have gotten"  ,correct, "question(s) right so far.")
 time.sleep(1.0)
 print("Question two! Is the fastest animal on the planet the cheetah? Type yes or no as an answer.")
 answer = input("")
@@ -36,6 +34,23 @@ if answer == "no":
     correct += 1
     print("Correct! The fastest animal on the planet is the peregrine falcon. You have gotten" ,correct, "question(s) right so far.")
 time.sleep(1.0)
+print("Question three! Are birds considered related to dinosaurs? Type yes or no as an answer.")
+answer = input("")
+if answer == "yes":
+    correct += 1
+    print("Correct! Birds are considered to be the relatives of dinosaurs! You have gotten" ,correct, "question(s) right so far.")
+if answer == "no":
+    correct += 0
+    print("That is incorrect. You have gotten" ,correct, "question(s) correct so far.")
+time.sleep(1.0)
+print("Question four! Are spiders considered insects? Type yes or no as an answer.")
+answer = input("")
+if answer == "yes":
+    correct += 0
+    print("That is incorrect. You have gotten" ,correct, "question(s) right so far.")
+if answer == "no":
+    correct += 1
+    print("Correct! Spiders are arachnids, which are technically not insects. You have gotten" ,correct, "question(s) right so far.")
 print("Final question! Are all mammals warm blooded? Type yes or no as an answer.")
 answer = input("")
 if answer == "yes":
